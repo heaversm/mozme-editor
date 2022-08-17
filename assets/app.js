ecce42f47115000611e67742cb6393ded741b8cf
Mike Heavers <mheavers@gmail.com>
2022-08-17 16:06:10 -0700

    fix stuff, add ejs

--- assets/app.js
+++ assets/app.js
@@ -1,13 +1,43 @@
-function compile() {
-  var html = document.getElementById("html");
-  var css = document.getElementById("css");
-  var js = document.getElementById("js");
-  var code = document.getElementById("code").contentWindow.document;
+const $codingArea = document.getElementById("coding_area");
+const $sourceButton = document.querySelector(".source-btn");
+
+const html = document.getElementById("html");
+const css = document.getElementById("css");
+const js = document.getElementById("js");
+const code = document.getElementById("code").contentWindow.document;
+
+let siteBoilerplate =
+  '<div id="container"><h2>Template Code</h2><div id="user-code"><%- userHTML %></div></div>';
+
+let htmlTemplate;
+
+function init() {
+  initialRender();
+  addListeners();
+}
+
+function renderHTMLTemplate() {
+  htmlTemplate = ejs.render(siteBoilerplate, { userHTML: html.value });
+}
+
+function initialRender() {
+  renderHTMLTemplate();
+  code.open();
+  code.writeln(htmlTemplate);
+  code.close();
+}
+
+function addListeners() {
+  $sourceButton.addEventListener("click", function () {
+    $codingArea.classList.toggle("js-hidden");
+  });
 
   document.body.onkeyup = function () {
+    renderHTMLTemplate();
+
     code.open();
     code.writeln(
-      html.value +
+      htmlTemplate +
         "<style>" +
         css.value +
         "</style>" +
@@ -18,4 +48,6 @@ function compile() {
     code.close();
   };
 }
-compile();
+
+function compile() {}
+init();
